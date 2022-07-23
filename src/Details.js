69cd7612143bf95b89a4412af4a89d8ec7c80d8f
alifakbar111 <alifakbar111@gmail.com>
2022-07-23 17:33:26 +0700

    feat: provider & dispatch

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,9 @@
 import { Component } from "react";
 import { useParams } from "react-router-dom";
+import { connect } from "react-redux";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
-import ThemeContext from "./ThemeContext";
+// import ThemeContext from "./ThemeContext";
 import Modal from "./Modal";
 
 class Details extends Component {
@@ -31,16 +32,12 @@ class Details extends Component {
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
-          <ThemeContext.Consumer>
-            {([theme]) => (
-              <button
-                onClick={this.toggleModal}
-                style={{ backgroundColor: theme }}
-              >
-                Adopt {name}
-              </button>
-            )}
-          </ThemeContext.Consumer>
+          <button
+            onClick={this.toggleModal}
+            style={{ backgroundColor: this.props.theme }}
+          >
+            Adopt {name}
+          </button>
           <p>{description}</p>
           {showModal ? (
             <Modal>
@@ -59,11 +56,14 @@ class Details extends Component {
   }
 }
 
+const mapStateProps = ({ theme }) => ({ theme })
+const ReduxWrappedDetails = connect(mapStateProps)(Details)
+
 const WrappedDetails = () => {
   const params = useParams();
   return (
     <ErrorBoundary>
-      <Details params={params} />
+      <ReduxWrappedDetails params={params} />
     </ErrorBoundary>
   );
 };
