7f5c944b486046106aca523214440fd5fd4a876d
alifakbar111 <alifakbar111@gmail.com>
2022-07-23 17:30:28 +0700

    feat: create store & reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+import location from "./location";
+import theme from "./theme";
+import animal from "./animal";
+import breed from "./breed";
+
+export default combineReducers({ location, theme, animal, breed });
